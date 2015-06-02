4d67feae739ff99074ff3cfdf4acfcd91e884828
Charlie Cheever <ccheever@gmail.com>
2015-06-02 03:17:57 -0700

    Currying resolved and rejected

---
+++ index.js
@@ -0,0 +1,33 @@
+var crayon = require('@ccheever/crayon');
+
+function resolved(opts) {
+  return function (result) {
+    opts = opts || {};
+    if (result != null) {
+      console.log(result);
+    }
+    if (opts.forceExit) {
+      process.exit(0);
+    }
+    return result;
+  }
+}
+
+function rejected(opts) {
+  return function (err) {
+    opts = opts || {};
+    crayon.red.error(err);
+    console.error(err.stack);
+    if (forceExit) {
+      process.exit(err.code || -1);
+    }
+    return err;
+  }
+}
+
+module.exports = function promisePrint(p, opts) {
+  return p.then(resolved(opts), rejected(opts));
+}
+
+module.exports.resolved = resolved;
+module.exports.rejected = rejected;
